3e641540dfbcf334377872db05da08837ed1b0cc
Clement <ongclement93@gmail.com>
2019-03-14 00:56:23 +0800

    Add migration files for table creating in database

---
+++ fbsapp/db/migrate/20190313164153_bookings.rb
@@ -0,0 +1,18 @@
+class Bookings < ActiveRecord::Migration[5.2]
+  def self.up
+    create_table :bookings do |t|
+      t.string     :bookingid,    :limit => 32, :null => false
+      t.string     :userid,       :limit => 32, :null => false
+      t.string     :roomid,       :limit => 32, :null => false
+      t.timestamp  :date
+      t.string     :startTime,    :limit => 4
+      t.string     :endTime,      :limit => 4
+      t.string     :status,       :limit => 20
+
+      t.timestamp  :created_at
+    end
+  end
+  def self.down
+    drop_table :rooms
+  end
+end
---
+++ fbsapp/db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_03_13_164153) do
+
+  create_table "bookings", options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "bookingid", limit: 32, null: false
+    t.string "userid", limit: 32, null: false
+    t.string "roomid", limit: 32, null: false
+    t.timestamp "date"
+    t.string "startTime", limit: 4
+    t.string "endTime", limit: 4
+    t.string "status", limit: 20
+    t.timestamp "created_at"
+  end
+
+  create_table "rooms", options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "roomid", limit: 32, null: false
+    t.string "name", limit: 32, null: false
+    t.text "description"
+    t.timestamp "created_at"
+  end
+
+  create_table "users", options: "ENGINE=MyISAM DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "userid", limit: 32, null: false
+    t.string "username", limit: 32, null: false
+    t.string "password", limit: 32, null: false
+    t.timestamp "created_at"
+  end
+
+end
