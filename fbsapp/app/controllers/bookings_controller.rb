ec8984f44c10ed140678762bf24f40d4d735d93e
Clement <ongclement93@gmail.com>
2019-03-14 15:06:41 +0800

    Add display current booking page, setup bootstrap for project and rectified error in routing

--- fbsapp/app/controllers/bookings_controller.rb
+++ fbsapp/app/controllers/bookings_controller.rb
@@ -1,11 +1,34 @@
 class BookingsController < ApplicationController
+    def index
+        @bookings = Booking.all
+    end
+
+    def show
+        @booking = Booking.find(params[:id])
+    end
+
+    def new
+        @booking = Booking.new
+    end
+    def edit
+        @booking = Booking.find(params[:id])
+    end
+
     def create
         @booking = Booking.new(booking_params)
         @booking.save
         redirect_back(fallback_location: root_path)
     end
+
+    def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+
+        redirect_back(fallback_location: root_path)
+    end
+
     private
     def booking_params
-        params.require(:booking).permit(:userid, :roomid, :date, :startTime, :endTime, :status)
+        params.require(:booking).permit(:id, :userid, :roomid, :date, :startTime, :endTime, :status)
     end
 end
--- fbsapp/config/routes.rb
+++ fbsapp/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
     get 'welcome/index'
 
-    resource :users
-    resource :rooms
-    resource :bookings
+    resources :users
+    resources :rooms
+    resources :bookings
 
     root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
