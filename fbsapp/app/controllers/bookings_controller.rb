9679ca5d1567dd3ced2c17329516fe3c2c114847
Clement <ongclement93@gmail.com>
2019-03-15 15:58:55 +0800

    Add validation check for new bookings

--- fbsapp/app/controllers/bookings_controller.rb
+++ fbsapp/app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
         if session[:username]=="admin"
             @bookings = Booking.all
         else
-            @bookings = Booking.where(["userid = ?", session[:user_id]]).first(5)
+            @bookings = Booking.where(["userid = ?", session[:user_id]])
         end
     end
 
@@ -20,9 +20,22 @@ class BookingsController < ApplicationController
 
     def create
         @booking = Booking.new(booking_params)
-        @booking.status = "Confirmed"
-        @booking.save
-        redirect_to(bookings_url)
+        @bookings = Booking.where(["date = ?", @booking.date])
+        overlap = false
+        @bookings.each do |bk|
+            if (bk.startTime...bk.endTime).overlaps?(@booking.startTime...@booking.endTime) and bk.roomid==@booking.roomid
+                overlap = true
+            end
+        end
+        if overlap
+            flash.now.alert  = 'Booking timing overlaps with current booking, please select a new timeslot'
+            render 'new'
+        else
+            @booking.userid = session[:user_id]
+            @booking.status = "Confirmed"
+            @booking.save
+            redirect_to(bookings_url)
+        end
     end
 
     def destroy
