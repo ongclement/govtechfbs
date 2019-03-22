134caf65c7c6ad0bdf074df52a3685ee3ab5ddc1
Clement <ongclement93@gmail.com>
2019-03-23 03:04:03 +0800

    Remove welcome page and route admin directly to booking management

--- fbsapp/app/controllers/sessions_controller.rb
+++ fbsapp/app/controllers/sessions_controller.rb
@@ -10,11 +10,7 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:session][:username].downcase)
         if user && user.password==params[:session][:password]
             log_in user
-            if user.username=="admin"
-                redirect_to(welcome_index_url)
-            else
-                redirect_to bookings_url
-            end
+            redirect_to bookings_url
         else
             if params[:session][:username].empty? or params[:session][:password].empty?
                 flash.now.alert  = 'Username/password cannot be empty'
